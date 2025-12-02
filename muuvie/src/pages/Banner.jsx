29bf265a1914da837e88242a3a3b1ac194822925
Vinicius Brito <viniciusbrito1@gmail.com>
2025-12-02 09:53:54 -0300

    ajuste na fonte + css banner

--- muuvie/src/pages/Banner.jsx
+++ muuvie/src/pages/Banner.jsx
@@ -21,6 +21,22 @@ function Banner() {
         <div className="container-fluid">
           <div className="col-lg-6 col-md-12">
             <div className="content"></div>
+            <img src="" alt="Movie Title" className="movie-title" />
+            <h4>
+              <span>Year</span>
+              <span>
+                <i>age</i>
+              </span>
+              <span>length</span>
+              <span>category</span>
+            </h4>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum
+              quaerat sapiente voluptatibus nobis voluptatum adipisci itaque
+              quia magni possimus omnis libero aspernatur dolorum vero obcaecati
+              commodi rem ratione, explicabo recusandae?
+            </p>
+            <div className="div buttom">Buttom</div>
           </div>
           <div className="col-lg-6 col-md-12"></div>
         </div>
